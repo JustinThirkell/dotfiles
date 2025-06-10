dcde4ccec746e514b587c844137a736e307a1b16
Justin Thirkell <justin.thirkell@vital.io>
2025-06-11 10:06:43 +1200

    turn off complete_aliases as causing git completion issues
    also move remaining zsh config into config file.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -31,7 +31,7 @@ setopt HIST_REDUCE_BLANKS
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
-setopt complete_aliases
+#setopt complete_aliases
 
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
@@ -41,3 +41,11 @@ bindkey '^[[3~' delete-char
 bindkey '^?' backward-delete-char
 
 ulimit -n 4096
+
+# Better history searching with arrow keys
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search   # Up arrow
+bindkey "^[[B" down-line-or-beginning-search # Down arrow
