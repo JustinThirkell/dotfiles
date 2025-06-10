df8e8b9b75723fdd6d011317eed8419af1812945
Justin Thirkell <justin.thirkell@vital.io>
2025-06-11 10:08:18 +1200

    comment out git completion function as competing with zsh and system git completion

--- git/completion.zsh
+++ git/completion.zsh
@@ -1,10 +1,17 @@
-# Uses git's autocompletion for inner commands. Assumes an install of git's
-# bash `git-completion` script at $completion below (this is where Homebrew
-# tosses it, at least).
-# CREDIT: https://github.com/holman/dotfiles/blob/master/git/completion.zsh
-completion='$(brew --prefix)/share/zsh/site-functions/_git'
-
-if test -f $completion
-then
-  source $completion
-fi
+# Git completion is handled by:
+# 1. System zsh git completion (/usr/share/zsh/5.9/functions/_git)
+# 2. Oh My Zsh git plugin
+#
+# Custom completion not needed - system + OMZ works perfectly!
+#
+# Original code kept for reference:
+#
+# #!/bin/zsh
+# # Uses git's autocompletion for inner commands. Assumes an install of git's
+# # bash `git-completion` script at $completion below (this is where Homebrew
+# # tosses it, at least).
+# # CREDIT: https://github.com/holman/dotfiles/blob/master/git/completion.zsh
+# completion='$(brew --prefix)/share/zsh/site-functions/_git'
+# if test -f $completion; then
+#   source $completion
+# fi
