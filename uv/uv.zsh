4b71767d5f0df6637116ebbc9a2fecc4ab207303
Justin Thirkell <justin.thirkell@vital.io>
2025-06-23 14:43:10 +1200

    only import uv env if exists

--- uv/uv.zsh
+++ uv/uv.zsh
@@ -1,3 +1,5 @@
-. "$HOME/.local/bin/env"
+if [ -f "$HOME/.local/bin/env" ]; then
+  . "$HOME/.local/bin/env"
+fi
 eval "$(uv generate-shell-completion zsh)"
 eval "$(uvx --generate-shell-completion zsh)"
