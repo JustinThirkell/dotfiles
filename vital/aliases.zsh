ff7de6f7f1ee4fc8def06bc3b2038add724386b1
Justin Thirkell <justin.thirkell@vital.io>
2025-07-08 15:01:29 +1200

    update app aliases

--- vital/aliases.zsh
+++ vital/aliases.zsh
@@ -3,18 +3,30 @@ VITAL_DIR=$PROJECTS/vital
 alias katoa='cd $VITAL_DIR/katoa'
 alias ddd='cd $VITAL_DIR/ddd'
 
+# Katoa => Care API
+alias apisst="yarn apps/care sst dev"
+alias apitype="yarn nx run care:type"
+alias apitest="yarn nx run care:test"
+alias apiinttest="yarn nx run care:integration-tests"
+
 # Katoa => Vital Console
-# alias cdev="yarn apps/console vite dev --mode test"
-# alias cui="rm -rf apps/console/dist && yarn apps/console playwright test --config playwright.ui.config.ts"
-# alias cco="rm -rf apps/console/playwright/.cache && yarn apps/console playwright test --config playwright.component.config.ts"
+alias csst="yarn apps/console sst dev"
+alias capp="yarn apps/console start"
+alias cfe="yarn apps/console vite dev --mode test"
+alias ctestui="rm -rf apps/console/dist && yarn apps/console playwright test --config playwright.ui.config.ts"
+alias ctestco="rm -rf apps/console/playwright/.cache && yarn apps/console playwright test --config playwright.component.config.ts"
 
 # Katoa => Vital Health
-alias hdev="yarn apps/health vite dev --mode test"
-alias hui="rm -rf apps/health/dist && yarn apps/health playwright test --config playwright.ui.config.ts"
-alias hco="rm -rf apps/health/playwright/.cache && yarn apps/health playwright test --config playwright.component.config.ts"
+alias hsst="yarn apps/health sst dev"
+alias happ="yarn apps/health start"
+alias hfe="yarn apps/health vite dev --mode test"
+alias htestui="rm -rf apps/health/dist && yarn apps/health playwright test --config playwright.ui.config.ts"
+alias htestco="rm -rf apps/health/playwright/.cache && yarn apps/health playwright test --config playwright.component.config.ts"
 
 # Katoa => Vital Patient
-alias pdev="yarn apps/patient vite dev --mode test"
+alias psst="yarn apps/patient sst dev"
+alias papp="yarn apps/patient start"
+alias pfe="yarn apps/patient vite dev --mode test"
 alias pui="rm -rf apps/patient/dist && yarn apps/patient playwright test --config playwright.ui.config.ts"
 alias pco="rm -rf apps/patient/playwright/.cache && yarn apps/patient playwright test --config playwright.component.config.ts"
 
