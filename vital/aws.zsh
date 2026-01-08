dcb33a5797bcf4c6a5a65263f6c0f3c19bf757cc
Justin Thirkell <justin.thirkell@gmail.com>
2026-01-09 10:07:25 +1300

    prevent old aws functions from overwriting cp ones

--- vital/aws.zsh
+++ vital/aws.zsh
@@ -1,28 +1,28 @@
 # https://app.gitbook.com/o/RC3jQcR2lEKirucc1K5J/s/diqdUiuDNfdDISnaesvl/aws/aws-vital-overview#aws-access-and-account-switching-on-vital-issued-laptops
 # https://docs.commonfate.io/granted/usage/assuming-roles
 
-function aws-sso() {
-  granted sso login --sso-region us-west-2 --sso-start-url "https://${AWS_APPS_IDENTIFIER}.awsapps.com/start/#"
-}
+# function aws-sso() {
+#   granted sso login --sso-region us-west-2 --sso-start-url "https://${AWS_APPS_IDENTIFIER}.awsapps.com/start/#"
+# }
 
-function aws-sso-refresh-roles() {
-  granted sso populate --sso-region us-west-2 "https://${AWS_APPS_IDENTIFIER}.awsapps.com/start/#"
-}
+# function aws-sso-refresh-roles() {
+#   granted sso populate --sso-region us-west-2 "https://${AWS_APPS_IDENTIFIER}.awsapps.com/start/#"
+# }
 
-function aws-dev() {
-  assume dev/SandboxAdmin --duration 8h
-}
+# function aws-dev() {
+#   assume dev/SandboxAdmin --duration 8h
+# }
 
-function aws-daily() {
-  aws-sso
-  aws-dev
-}
+# function aws-daily() {
+#   aws-sso
+#   aws-dev
+# }
 
-function aws-creds-clear() {
-  granted cache clear --storage=sso-token
-  granted cache clear --storage=session-credentials
-}
+# function aws-creds-clear() {
+#   granted cache clear --storage=sso-token
+#   granted cache clear --storage=session-credentials
+# }
 
-function aws-check-session() {
-  aws sts get-caller-identity
-}
+# function aws-check-session() {
+#   aws sts get-caller-identity
+# }
